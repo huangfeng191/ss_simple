c22173844a5dba7cbe4f45000c8d5e7abe27d885
fengfeng <huangfeng191@163.com>
2017-10-10 17:03:22 +0800

    1

--- src/nouse/cs.vue
+++ src/nouse/cs.vue
@@ -1,29 +1,42 @@
 <template>
-    <div id="cs"  >
-      
+    <div id="cs">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane   name="first">
+               <span slot="label"><i class="el-icon-date"></i> 我的行程</span>
+                <el-row>
+                    <el-col :span="4">
+                        <div>1</div>
+                    </el-col>
+                    <el-col :span="4">
+                        <div>2</div>
+                    </el-col>
+                </el-row>
+            </el-tab-pane>
+            <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+            <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+            <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-
-
+            activeName: 'second'
         };
     },
     methods: {
-
+        handleClick(tab, event) {
+            console.log(tab, event);
+        }
     },
+
     watch: {
 
     },
     mounted() {}
 };
-
- 
 </script>
 <style lang="less">
-#cs {
-    
-}
+#cs {}
 </style>
