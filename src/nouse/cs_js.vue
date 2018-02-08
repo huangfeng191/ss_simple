51a052f23287d203b5a82728f6ad0e8d0a8e2b0f
fengfeng <huangfeng191@163.com>
2018-02-08 10:36:17 +0800

    all

--- src/nouse/cs_js.vue
+++ src/nouse/cs_js.vue
@@ -1,40 +1,67 @@
+
 <template>
-	<div id="cs2">
-    <el-label> 计划输入方法体，返回打印控制台</el-label>
-    <el-input   type="textarea" v-model="forInput" ></el-input>
-    <el-label> 为了打印控制台</el-label>
-    <el-input   type="textarea" v-model="forCon"></el-input>
-	</div>
-
-</template>
-<script>
-export default {
-  data() {
-    return {
-      forCon: "",
-      forCon: "控制台显示",
-      lvl: 1
+        <div id="cs2">
+        <el-row>
+            <el-col :span="12">
+                <span> 计划输入方法体，返回打印控制台</span>
+                <el-input   type="textarea" v-model="forInput" :autosize="{ minRows: 18}" ></el-input>
+            </el-col>
+            <el-col :span="12">
+                <span> 为了打印控制台</span>
+                <el-input   type="textarea" v-model="forCon" :autosize="{ minRows: 18}"></el-input>
+            </el-col>
+        </el-row>
+        
+        </div>
+    
+    </template>
+    <script>
+    export default {
+      data() {
+        return {
+          print: [],
+          forInput: "",
+          forCon: "控制台显示",
+          lvl: 1
+        };
+      },
+      methods: {
+        // 可以考虑添加 try catch
+        showDetail: function(a) {
+          let self = this;
+          self.print = [];
+          function p(a) {
+            self.print.push(a);
+          }
+          var strFunciton =
+            `
+            function play(log){` +
+            ` log.push("p -value-");
+         ` +
+            self.forInput +
+          `  log.push("end;")}
+         `;
+          debugger;
+          //  var ss = bb;
+          var o = { getplay: eval("(" + strFunciton + ")") };
+          o.getplay(self.print);
+    
+          debugger;
+          return a;
+        }
+      },
+      watch: {
+        forInput: function(val, oldVal) {
+          let self = this;
+          self.showDetail(self.print);
+          self.forCon = self.print.join("\n");
+        }
+      },
+      mounted() {},
+      components: {}
     };
-  },
-  methods: {},
-  watch: {},
-  mounted() {
-    function showDetail(a) {
-      $.each(a, function(ai, av) {
-        return;
-      });
-      a.push(2);
-      return;
+    </script>
+    <style lang="less">
+    #cs2 {
     }
-
-    let a = [1];
-    showDetail(a);
-    this.forCon = a;
-  },
-  components: {}
-};
-</script>
-<style lang="less">
-#cs2 {
-}
-</style>
+    </style>
