03350f3884df12ab02e9c67bcd1ac4eb7e27a813
fengfeng <huangfeng191@163.com>
2018-10-14 19:46:37 +0800

    sdf

--- src/customs/public/usejsonStock.vue
+++ src/customs/public/usejsonStock.vue
@@ -14,7 +14,7 @@ export default {
       // selected: ["scada6crudcol", "scada6crudinputOne"],
       // selected: ["scada6crudinputThree", "toRowSingle", "scada6crudcol", "scada6Input"],
       // selected: ["scada6crudinputThree", "toRowSingle", "CueColumns", "CueCrudInputThree"],
-      selected: [ "proStockCol","scada6crudinputThree", "scada6crudcol"],
+      selected: [ "proStockCol","scada6crudinputThree", "scada6crudcol","scada6crudprop","scada6crudquics"],
        
       types: [
             {
@@ -28,6 +28,114 @@ export default {
             roles: [],
             fixRoles: []
           }
+        },
+         {
+          value: "scada6crudquics",
+          label: "scada6crudquics",
+          template:
+            '{ "Field": "${1:nm}", "Label": "${0:nm}", "Type":  "QCombox",  TextField: "name", ValueField: "value", "Width": 90 ,${31} },',
+          param: [
+            
+            {
+              k: "3",
+              v: [
+                { k: "filterStr", v: [{ k: "notNumber", operate: "and" }] },
+                { k: "replace", v: { c: "QCombox", d: "QDatetime", r: "QDatetimeRange",  t: "text" } }
+              ]
+            },
+            {
+              k: "31",
+              v: [
+                { k: "copy", v: { "3": true }, scope: ["c","d","r"] },
+                {
+                  k: "containsReplace",
+                  v: {
+                    c: {
+                      k: "fun",
+                      v: function(row, strLikeObject) {
+                        if (row[4]) {
+                          return ' Ext: "'+row[4]+'", Source: "'+row[4]+'", ' ;
+                        } else {
+                          return ' Ext: "USER", Source: "USER", ';
+                        }
+                      }
+                    },
+                     d: {
+                      k: "fun",
+                      v: function(row, strLikeObject) {
+                        if (row[4] && /^[0-9]+$/g.exec(row[4]) == null) {
+                          return 'Ext: "{Format:\"'+row[4]+'\"}", Value: new Date().FormatString("'+row[4]+'")';
+                        } else {
+                          return 'Ext: "{Format:\"yyyy-MM-dd\"}", Value: new Date().FormatString("yyyy-MM-dd")';
+                        }
+                      }
+                    },
+                     r: {
+                      k: "fun",
+                      v: function(row, strLikeObject) {
+                        if (row[4] && /^[0-9]+$/g.exec(row[4]) == null) {
+                          return 'Ext: "{Format:\"'+row[4]+'\"}",Begin: new Date().AddMonths(-1).FormatString("'+row[4]+'"), End: new Date().FormatString("'+row[4]+'")'   ;
+                        } else {
+                          return 'Ext: "{Format:\"yyyy-MM-dd\"}",Begin: new Date().AddMonths(-1).FormatString("yyyy-MM-dd"), End: new Date().FormatString("yyyy-MM-dd")';
+                        }
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          ],
+        },
+
+         {
+          value: "scada6crudprop",
+          label: "scada6crudprop",
+          template:
+            '{ "Field": "${1}", "Name": "${0}", ShowType: "${3:text}", Ext: "${31}",DataType: "${2:String}", "Unique": 0, "FilterEnabled": false, "Frozen": false, "Hidden": false, "OrderEnabled": false  },',
+          param: [
+            {
+              k: "2",
+              v: [{ k: "replace", v: { string: "String", int: "Number", double: "Number" } }]
+            },
+            {
+              k: "3",
+              v: [
+                { k: "filterStr", v: [{ k: "notNumber", operate: "and" }] },
+                { k: "replace", v: { c: "combo", d: "datetime", a: "textarea", u: "upload", t: "text" } }
+              ]
+            },
+            {
+              k: "31",
+              v: [
+                { k: "copy", v: { "3": true }, scope: ["c","d"] },
+                {
+                  k: "containsReplace",
+                  v: {
+                    c: {
+                      k: "fun",
+                      v: function(row, strLikeObject) {
+                        if (row[4]) {
+                          return row[4];
+                        } else {
+                          return "USER";
+                        }
+                      }
+                    },
+                     d: {
+                      k: "fun",
+                      v: function(row, strLikeObject) {
+                        if (row[4] && /^[0-9]+$/g.exec(row[4]) == null) {
+                          return row[4];
+                        } else {
+                          return "yyyy-MM-dd";
+                        }
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          ],
         },
 
          {
