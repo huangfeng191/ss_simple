a681a14047f8948d9052581b6615bb19920cade2
fengfeng <huangfeng191@163.com>
2020-03-05 17:42:01 +0800

    a

--- src/customs/public/usejsonTide.vue
+++ src/customs/public/usejsonTide.vue
@@ -14,47 +14,51 @@ export default {
   props: {},
   data() {
     return {
-      selected: [
-
-        "CrudInputs3",
-        "CrudColumns",
-        "CrudProp",
-        "CrudQueries"
-      ],
+      selected: ["CrudInputs3", "CrudColumns", "CrudProp", "CrudQueries"],
 
       types: [
         {
           value: "CrudColumns",
           label: "CrudColumns",
-          template: '{ "field": "${1:sn}","title": "${0:nm}", "width": 100,$?param1? },',
-          mDParamHandleBefore:function({param,v,aRow}){
-              
-              if(param=="param1" && aRow.length>2 && aRow[2]=="d"){
-                if(!v){
-                    v="formatter: DateFormatter,"
-                }else if(v=="yyyy"){
-                    v="formatter: YearFormatter,"
-                }else if(v=="yyyy-MM"){
-                    v="formatter: MonthFormatter,"
-                }else{
-                   v='formatter:"'+v+'"'
+          template:
+            '{ "field": "${1:sn}","title": "${0:nm}", "width": 100,$?param1? },',
+          protoRowTranslate: [
+            {
+              k: "fun",
+              v: function(arr, index, self) {
+                let MDParam = self.protoParam.MDTitle;
+                if (MDParam.length > 0) {
+                  if (MDParam[index] && MDParam[index][0]) {
+                    arr[1] = MDParam[index][0] + "." + (arr[1] || "");
+                  }
                 }
-                 
-              }else if(param=="param1" && aRow.length>2 && aRow[2]=="c"){
-                debugger
-                    v='binding:"'+(v||"USER")+'"'
-                 
-              }else if(param=="param1" && aRow.length>2 && aRow[2]=="f"){
-                 v="formatter: function (v, r, i) {\n if (v){\n return v; \n}else {\n return v; \n}   },"
-                
-              }else if(param=="param1"){
-                v=""
+                return arr;
               }
+            }
+          ],
+          mDParamHandleBefore: function({ param, v, aRow }) {
+            if (param == "param1" && aRow.length > 2 && aRow[2] == "d") {
+              if (!v) {
+                v = "formatter: DateFormatter,";
+              } else if (v == "yyyy") {
+                v = "formatter: YearFormatter,";
+              } else if (v == "yyyy-MM") {
+                v = "formatter: MonthFormatter,";
+              } else {
+                v = 'formatter:"' + v + '"';
+              }
+            } else if (param == "param1" && aRow.length > 2 && aRow[2] == "c") {
+              debugger;
+              v = 'binding:"' + (v || "USER") + '"';
+            } else if (param == "param1" && aRow.length > 2 && aRow[2] == "f") {
+              v =
+                "formatter: function (v, r, i) {\n if (v){\n return v; \n}else {\n return v; \n}   },";
+            } else if (param == "param1") {
+              v = "";
+            }
 
-
-              return v ;
-
-            },
+            return v;
+          }
         },
 
         {
@@ -67,29 +71,45 @@ export default {
             {
               k: "2",
               v: [
-                { k: "replace", v: { c: "combo", d: "datetime", a: "textarea", u: "upload", t: "text", f: "text" } }
+                {
+                  k: "replace",
+                  v: {
+                    c: "combo",
+                    d: "datetime",
+                    a: "textarea",
+                    u: "upload",
+                    t: "text",
+                    f: "text"
+                  }
+                }
               ]
-            },
-           
+            }
           ],
           protoRowTranslate: [
-    
+            {
+              k: "fun",
+              v: function(arr, index, self) {
+                let MDParam = self.protoParam.MDTitle;
+                if (MDParam.length > 0) {
+                  if (MDParam[index] && MDParam[index][0]) {
+                    arr[1] = MDParam[index][0] + "." + (arr[1] || "");
+                  }
+                }
+                return arr;
+              }
+            }
           ],
-            mDParamHandleBefore:function({param,v,aRow}){
-              
-              if(param=="param1" && aRow.length>2 && aRow[2]=="d"&&!v){
-                  v="yyyy-MM-dd"
-              }else  if(param=="param1" && aRow.length>2 && aRow[2]=="d"){
-                 
-              }else if(param=="param1" && aRow.length>2 && aRow[2]=="c"){
-                  v=v||"USER"
-              }
-              else if(param=="param1"){
-                 v=""
-              }
-              return v ;
-
-            },
+          mDParamHandleBefore: function({ param, v, aRow }) {
+            if (param == "param1" && aRow.length > 2 && aRow[2] == "d" && !v) {
+              v = "yyyy-MM-dd";
+            } else if (param == "param1" && aRow.length > 2 && aRow[2] == "d") {
+            } else if (param == "param1" && aRow.length > 2 && aRow[2] == "c") {
+              v = v || "USER";
+            } else if (param == "param1") {
+              v = "";
+            }
+            return v;
+          },
 
           deakTemplateLikeArray: function(a, self) {
             // 对生成后的行数据数组,再次处理
@@ -108,7 +128,11 @@ export default {
                 a[i] = a[i].replace(/^{/, "[{");
               }
               group += Number(v);
-              if (i == ids.length - 1 || group + ids[i + 1] > 3 || (o[i] && o[i].br)) {
+              if (
+                i == ids.length - 1 ||
+                group + ids[i + 1] > 3 ||
+                (o[i] && o[i].br)
+              ) {
                 a[i] = a[i].replace(/},$/, "},],");
                 group = 0;
               }
@@ -124,32 +148,52 @@ export default {
         {
           value: "CrudProp",
           label: "CrudProp",
-          template: '{ "Field": "${1}", "Name": "${0}", ShowType: "${2:text}", Ext: "$?param1?",DataType: "$?dataType:String?" },',
-         
+          template:
+            '{ "Field": "${1}", "Name": "${0}", ShowType: "${2:text}", Ext: "$?param1?",DataType: "$?dataType:String?" },',
+
           param: [
             {
               k: "2",
               v: [
-                { k: "replace", v: { c: "combo", d: "datetime", a: "textarea", u: "upload", t: "text", f: "text" } }
+                {
+                  k: "replace",
+                  v: {
+                    c: "combo",
+                    d: "datetime",
+                    a: "textarea",
+                    u: "upload",
+                    t: "text",
+                    f: "text"
+                  }
+                }
               ]
-            },
-           
+            }
           ],
-
-         mDParamHandleBefore:function({param,v,aRow}){
-              
-            if(param=="param1" && aRow.length>2 && aRow[2]=="d"&&!v){
-                  v="yyyy-MM-dd"
-              }else  if(param=="param1" && aRow.length>2 && aRow[2]=="d"){
-                 
-              }else if(param=="param1" && aRow.length>2 && aRow[2]=="c"){
-                   v=v||"USER"
-              }else  if(param=="param1"){
-                 v=""
+          protoRowTranslate: [
+            {
+              k: "fun",
+              v: function(arr, index, self) {
+                let MDParam = self.protoParam.MDTitle;
+                if (MDParam.length > 0) {
+                  if (MDParam[index] && MDParam[index][0]) {
+                    arr[1] = MDParam[index][0] + "." + (arr[1] || "");
+                  }
+                }
+                return arr;
               }
-              return v ;
-
-            },
+            }
+          ],
+          mDParamHandleBefore: function({ param, v, aRow }) {
+            if (param == "param1" && aRow.length > 2 && aRow[2] == "d" && !v) {
+              v = "yyyy-MM-dd";
+            } else if (param == "param1" && aRow.length > 2 && aRow[2] == "d") {
+            } else if (param == "param1" && aRow.length > 2 && aRow[2] == "c") {
+              v = v || "USER";
+            } else if (param == "param1") {
+              v = "";
+            }
+            return v;
+          }
         },
 
         {
@@ -157,42 +201,62 @@ export default {
           label: "CrudQueries",
           template:
             '{ "Field": "${1:nm}", "Label": "${0:nm}", "Type":  "${3:QText}", $?param1? },',
-                  
+
           param: [
             {
               k: "2",
               v: [
-                { k: "replace", v: { c: "combo", d: "datetime", a: "textarea", u: "upload", t: "text" } }
+                {
+                  k: "replace",
+                  v: {
+                    c: "combo",
+                    d: "datetime",
+                    a: "textarea",
+                    u: "upload",
+                    t: "text"
+                  }
+                }
               ]
-            },
-           
+            }
           ],
 
-         mDParamHandleBefore:function({param,v,aRow}){
-        
-        
-
-              if(param=="param1" && aRow.length>2 && aRow[2]=="d"){
-               
-                 v='Ext: "{Format:"' + (v||"yyyy-MM-dd") + '"}", Value: new Date().FormatString("' + (v||"yyyy-MM-dd") + '")';
-                 
-              }else if(param=="param1" && aRow.length>2 && aRow[2]=="c"){
-                 v=' Ext: "' + (v||"USER") + '", Source: "'  + (v||"USER") + '", ' 
-              }else  if(param=="param1"){
-                 v=""
+          protoRowTranslate: [
+            {
+              k: "fun",
+              v: function(arr, index, self) {
+                let MDParam = self.protoParam.MDTitle;
+                if (MDParam.length > 0) {
+                  if (MDParam[index] && MDParam[index][0]) {
+                    arr[1] = MDParam[index][0] + "." + (arr[1] || "");
+                  }
+                }
+                return arr;
               }
-
-              
-
-
-              return v ;
-
-            },
-        },
-            
+            }
           ],
-  
-       
+          mDParamHandleBefore: function({ param, v, aRow }) {
+            if (param == "param1" && aRow.length > 2 && aRow[2] == "d") {
+              v =
+                'Ext: "{Format:"' +
+                (v || "yyyy-MM-dd") +
+                '"}", Value: new Date().FormatString("' +
+                (v || "yyyy-MM-dd") +
+                '")';
+            } else if (param == "param1" && aRow.length > 2 && aRow[2] == "c") {
+              v =
+                ' Ext: "' +
+                (v || "USER") +
+                '", Source: "' +
+                (v || "USER") +
+                '", ';
+            } else if (param == "param1") {
+              v = "";
+            }
+
+            return v;
+          }
+        }
+      ]
     };
   },
   created() {},
